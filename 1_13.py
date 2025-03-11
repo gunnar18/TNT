a533e8f9a8563ed5e1677f3d14a0165e0acad62e
gunnar18 <gunnaregillg@gmail.com>
2025-03-11 10:30:58 +0000

    Add files via upload

---
+++ 1_13.py
@@ -0,0 +1,4 @@
+lengd = int(input("Sláðu inn lengd rétthyrningsins í cm:")) #biður um lengd í cm
+breidd = int(input("Sláðu inn breidd rétthyrningsins í cm:")) #biður um breidd í cm
+flatarmal = lengd * breidd #reiknar flatarmal Lengd*breidd
+print(F"Flatarmál rétthyrningsins er: {flatarmal}cm2") #gefur upp flatarmál í cm
